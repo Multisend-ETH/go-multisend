bd38dc9557c8af83f1ee1c99d03ff6ac2381fa76
eltneg <yorewole@gmail.com>
2021-02-16 10:43:31 +0100

    Fix typo

--- multisendvy/multisendvy.go
+++ multisendvy/multisendvy.go
@@ -6,8 +6,8 @@ import (
 	"github.com/ethereum/go-ethereum"
 )
 
-// MultsendEtherData send ether to multiple addresses
-func MultsendEtherData(ctx context.Context, addresses [100]string, amounts [100]float64) *ethereum.CallMsg {
+// MultisendEtherData send ether to multiple addresses
+func MultisendEtherData(ctx context.Context, addresses [100]string, amounts [100]float64) *ethereum.CallMsg {
 	methodID := getMethodID(multiSendEtherABI)
 
 	byteAmount, etherValue := getEncodedAmounts(amounts)
@@ -24,8 +24,8 @@ func MultsendEtherData(ctx context.Context, addresses [100]string, amounts [100]
 	return &data
 }
 
-// MultsendWeiData send ether to multiple addresses
-func MultsendWeiData(ctx context.Context, addresses [100]string, weiAmounts [100]string) *ethereum.CallMsg {
+// MultisendWeiData send ether to multiple addresses
+func MultisendWeiData(ctx context.Context, addresses [100]string, weiAmounts [100]string) *ethereum.CallMsg {
 	methodID := getMethodID(multiSendEtherABI)
 
 	byteAmount, etherValue := getEncodedWeiAmounts(weiAmounts)
